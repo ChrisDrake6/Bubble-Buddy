e703d2f5bf1238502b377b16ff5e8b08061a341c
robinll <robin.laws@web.de>
2025-01-25 00:55:02 +0100

    Bubble with simple leash (without sprites)

--- Assets/Scripts/Bubblebehaviour.cs
+++ Assets/Scripts/Bubblebehaviour.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class Bubblebehaviour : MonoBehaviour
 {
-    [SerializeField] Player player;
-    [SerializeField] float lineLength;
+    [SerializeField] private Player player;
+    [SerializeField] private float lineLength;
+    [SerializeField] private float lineforce;
+    private new Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Linestop();
+        Debug.Log((transform.position - player.transform.position).magnitude);
+    }
 
     private void Linestop()
     {
         if ((transform.position - player.transform.position).magnitude > lineLength)
         {
-
+            //transform.position = lineLength * (-player.transform.position + transform.position).normalized;
+            rigidbody2D.AddForce(lineforce * -transform.position + player.transform.position, ForceMode2D.Impulse);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, player.transform.position);
     }
 }
